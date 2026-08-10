6b366376e4ba781df292099613846e9ad5031c1f
evillypaiva-png <evilly.paiva@escola.pr.gov.br>
2026-08-10 12:28:08 -0300

    10/08/2026

---
+++ script.js
@@ -0,0 +1,9 @@
+button{
+    padding:10px 20px;
+    font-size:18px;
+    cursor:pointer;
+    }
+    const botao=document.getElementByld("tema");
+    botao.addEventListener("click",function(){
+        document.body.classList.toggle("dark");
+        });
